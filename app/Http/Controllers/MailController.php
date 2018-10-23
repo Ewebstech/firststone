81ac250907b01b4b9f756a9356c45068bef7402d
Emmanuel Osaigbovo <e.osaigbovo@dreammesh.ng>
2018-10-23 17:05:53 +0100

    minor chnage

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Validator;
 class MailController extends Controller
 {
     public function store(Request $request) {
+        $message = [
+            'email.unique' => 'Email is already subscribed'
+        ];
         Validator::make($request->all(), [
-            'email' => 'required|email'
-        ])->validate();
+            'email' => 'required|email|unique:mails'
+        ], $message)->validate();
 
         $email = new Mail;
         $email->email = $request->email;

0672d946346f4b0189229f43d892eda4414cd920
Emmanuel Paul <e.paul@dreammesh.ng>
2018-11-02 10:23:03 +0100

    seed update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
-            PropertyTableSeeder::class,
+            //PropertyTableSeeder::class,
         ]);
     }
 }

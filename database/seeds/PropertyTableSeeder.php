9be7dbcde00a966a3ce05e5489a7957aca970b87
Emmanuel Paul <e.paul@dreammesh.ng>
2018-11-02 10:12:20 +0100

    seed update

--- database/seeds/PropertyTableSeeder.php
+++ database/seeds/PropertyTableSeeder.php
@@ -20,7 +20,8 @@ class PropertyTableSeeder extends Seeder
                 'amount' => '5000000',
                 'type' => 'default',
                 'description' => 'This is a 5 bedroom, 1.5 bathroom, single family home. It is spacious and located in Lagos.',
-                'image_name' => '1.jpg',   
+                'image_name' => '1.jpg', 
+                'thumbnail_image' => 'test.jpg'  
             ],
 
             [
@@ -30,7 +31,8 @@ class PropertyTableSeeder extends Seeder
                 'amount' => '5000000',
                 'type' => 'default',
                 'description' => 'This is a 5 bedroom, 1.5 bathroom, single family home. It is spacious and located in Lagos.',
-                'image_name' => '2.jpg',   
+                'image_name' => '2.jpg',  
+                'thumbnail_image' => 'test.jpg'  
             ],
 
             [
@@ -41,6 +43,7 @@ class PropertyTableSeeder extends Seeder
                 'type' => 'default',
                 'description' => 'This is a 5 bedroom, 1.5 bathroom, single family home. It is spacious and located in Lagos.',
                 'image_name' => '3.jpg',   
+                'thumbnail_image' => 'test.jpg'  
             ],
 
         ];
